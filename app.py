7047cb0c3ac039e542d264a917757a56e11788f6
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-17 16:29:39 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,56 @@
-import os
-import ccxt
 import json
 from flask import Flask, request
+import ccxt
 
 app = Flask(__name__)
 
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-pending_orders = []
+exchange = ccxt.binance({
+    'apiKey': config['EXCHANGES']['BINANCE-FUTURES']['API_KEY'],
+    'secret': config['EXCHANGES']['BINANCE-FUTURES']['API_SECRET'],
+    'options': {
+        'defaultType': 'future',
+    },
+    'urls': {
+        'api': {
+            'public': 'https://fapi.binance.com/fapi/v1',
+            'private': 'https://fapi.binance.com/fapi/v1',
+        },
+    }
+})
 
-@app.route('/')
-def index():
-    return {'message': 'Server is running!'}
+class Bot:
+
+    def __init__(self):
+        pass
+
+    def close_position(self, symbol, side):
+        position = exchange.fetch_positions(symbol)[0]['info']['positionAmt']
+        params = {
+            'reduceOnly': True
+        }
+        if side == 'long':
+            exchange.create_order(symbol, 'Market', 'Sell', float(position), price=None, params=params)
+        else:
+            exchange.create_order(symbol, 'Market', 'Buy', -float(position), price=None, params=params)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
-    print(f"Received data: {data}")
-
-    if not validate_data(data):
-        return "Invalid data", 400
-
-    try:
-        execute_trade(data)
-    except Exception as e:
-        print(f"Error executing trade: {e}")
-        return "Error executing trade", 500
-
-    return "Success", 200
-
-def validate_data(data):
-    if 'exchange' not in data or data['exchange'] not in config['EXCHANGES']:
-        return False
-    return True
-
-def execute_trade(data):
-    exchange = getattr(ccxt, data['exchange'].lower())({
-        'apiKey': config['EXCHANGES'][data['exchange']]['API_KEY'],
-        'secret': config['EXCHANGES'][data['exchange']]['API_SECRET'],
-    })
-
-    if config['EXCHANGES'][data['exchange']].get('TESTNET', False):
-        exchange.set_sandbox_mode(True)
-
     symbol = data['symbol']
-    market = data['market']
-    order_type = data['type']
+    side = data['side']
+    bot = Bot()
 
-    position = exchange.fetch_position(symbol)
-    position_open = position['side'] != 'none'
+    if side == 'Buy' or side == 'Sell':
+        exchange.create_order(symbol, 'Market', side)
+    elif side == 'closelong':
+        bot.close_position(symbol, 'long')
+    elif side == 'closeshort':
+        bot.close_position(symbol, 'short')
 
-    if order_type == 'buy' or order_type == 'sell':
-        if not position_open:
-            if order_type == 'buy':
-                exchange.create_market_buy_order(symbol, market)
-            else:
-                exchange.create_market_sell_order(symbol, market)
-
-            pending_orders.clear()
-        else:
-            print("Position is already open. Ignoring the new order.")
-    elif order_type == 'close-long':
-        if position_open and position['side'] == 'long':
-            exchange.create_market_sell_order(symbol, position['size'])
-    elif order_type == 'close-short':
-        if position_open and position['side'] == 'short':
-            exchange.create_market_buy_order(symbol, position['size'])
-    else:
-        print("Invalid order type")
+    return {'status': 'success'}
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
