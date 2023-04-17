c8fdde7d03f4e7b5d583c37e0be975726c5909e3
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-18 01:47:57 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -1,56 +1,85 @@
 import json
 from flask import Flask, request
+from pybit import HTTP
 import ccxt
+from binanceFutures import handle_binance_request
+from bybit_handler import handle_bybit_request
 
 app = Flask(__name__)
 
+# load config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-exchange = ccxt.binance({
-    'apiKey': config['EXCHANGES']['BINANCE-FUTURES']['API_KEY'],
-    'secret': config['EXCHANGES']['BINANCE-FUTURES']['API_SECRET'],
-    'options': {
-        'defaultType': 'future',
-    },
-    'urls': {
-        'api': {
-            'public': 'https://fapi.binance.com/fapi/v1',
-            'private': 'https://fapi.binance.com/fapi/v1',
+use_bybit = config['EXCHANGES']['BYBIT']['ENABLED']
+if use_bybit:
+    session = HTTP(
+        endpoint='https://api.bybit.com',
+        api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
+        api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
+    )
+
+use_binance_futures = config['EXCHANGES']['BINANCE-FUTURES']['ENABLED']
+if use_binance_futures:
+    exchange = ccxt.binance({
+        'apiKey': config['EXCHANGES']['BINANCE-FUTURES']['API_KEY'],
+        'secret': config['EXCHANGES']['BINANCE-FUTURES']['API_SECRET'],
+        'options': {
+            'defaultType': 'future',
         },
-    }
-})
+    })
+    if config['EXCHANGES']['BINANCE-FUTURES']['TESTNET']:
+        exchange.set_sandbox_mode(True)
 
-class Bot:
-
-    def __init__(self):
-        pass
-
-    def close_position(self, symbol, side):
-        position = exchange.fetch_positions(symbol)[0]['info']['positionAmt']
-        params = {
-            'reduceOnly': True
-        }
-        if side == 'long':
-            exchange.create_order(symbol, 'Market', 'Sell', float(position), price=None, params=params)
-        else:
-            exchange.create_order(symbol, 'Market', 'Buy', -float(position), price=None, params=params)
+@app.route('/')
+def index():
+    return {'message': 'Server is running!'}
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.get_json()
-    symbol = data['symbol']
-    side = data['side']
-    bot = Bot()
+    print("Hook Received!")
+    data = json.loads(request.data)
+    print(data)
 
-    if side == 'Buy' or side == 'Sell':
-        exchange.create_order(symbol, 'Market', side)
-    elif side == 'closelong':
-        bot.close_position(symbol, 'long')
-    elif side == 'closeshort':
-        bot.close_position(symbol, 'short')
+    if int(data['key']) != config['KEY']:
+        print("Invalid Key, Please Try Again!")
+        return {
+            "status": "error",
+            "message": "Invalid Key, Please Try Again!"
+        }
 
-    return {'status': 'success'}
+    if data['exchange'] == 'bybit':
+        if use_bybit:
+            handle_bybit_request(session, data)
+            return {
+                "status": "success",
+                "message": "Bybit Webhook Received!"
+            }
+        else:
+            return {
+                "status": "error",
+                "message": "Bybit is not enabled in the configuration."
+            }
+
+    if data['exchange'] == 'binance-futures':
+        if use_binance_futures:
+            handle_binance_request(exchange, data)
+            return {
+                "status": "success",
+                "message": "Binance Futures Webhook Received!"
+            }
+        else:
+            return {
+                "status": "error",
+                "message": "Binance Futures is not enabled in the configuration."
+            }
+
+    else:
+        print("Invalid Exchange, Please Try Again!")
+        return {
+            "status": "error",
+            "message": "Invalid Exchange, Please Try Again!"
+        }
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
---
+++ bybit_handler.py
@@ -0,0 +1,29 @@
+from pybit import HTTP
+
+def handle_bybit_request(session: HTTP, data: dict):
+    action = data['action']
+
+    if action == 'create_order':
+        create_order(session, data)
+    else:
+        print("Invalid action for Bybit")
+
+def create_order(session: HTTP, data: dict):
+    symbol = data['symbol']
+    order_type = data['order_type']
+    side = data['side']
+    qty = data['qty']
+    price = data['price']
+
+    try:
+        result = session.place_active_order(
+            symbol=symbol,
+            side=side,
+            order_type=order_type,
+            qty=qty,
+            price=price,
+            time_in_force="GoodTillCancel"
+        )
+        print("Order created:", result)
+    except Exception as e:
+        print("Error creating order:", e)
