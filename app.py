786e83be0bef85f02aed1bbe6dd7f291cb79ed87
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-04-18 03:01:57 +0330

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ with open('config.json') as config_file:
     config = json.load(config_file)
 
 use_bybit = config['EXCHANGES']['BYBIT']['ENABLED']
+if use_bybit:
+    session = HTTP(
+        endpoint='https://api.bybit.com',
+        api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
+        api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
+    )
 
 use_binance_futures = config['EXCHANGES']['BINANCE-FUTURES']['ENABLED']
 if use_binance_futures:
@@ -24,4 +30,55 @@ if use_binance_futures:
     if config['EXCHANGES']['BINANCE-FUTURES']['TESTNET']:
         exchange.set_sandbox_mode(True)
 
-# ... rest of the code ...
+@app.route('/')
+def index():
+    return {'message': 'Server is running!'}
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print("Hook Received!")
+    data = json.loads(request.data)
+    print(data)
+
+    if int(data['key']) != config['KEY']:
+        print("Invalid Key, Please Try Again!")
+        return {
+            "status": "error",
+            "message": "Invalid Key, Please Try Again!"
+        }
+
+    if data['exchange'] == 'bybit':
+        if use_bybit:
+            handle_bybit_request(session, data)
+            return {
+                "status": "success",
+                "message": "Bybit Webhook Received!"
+            }
+        else:
+            return {
+                "status": "error",
+                "message": "Bybit is not enabled in the configuration."
+            }
+
+    if data['exchange'] == 'binance-futures':
+        if use_binance_futures:
+            handle_binance_request(exchange, data)
+            return {
+                "status": "success",
+                "message": "Binance Futures Webhook Received!"
+            }
+        else:
+            return {
+                "status": "error",
+                "message": "Binance Futures is not enabled in the configuration."
+            }
+
+    else:
+        print("Invalid Exchange, Please Try Again!")
+        return {
+            "status": "error",
+            "message": "Invalid Exchange, Please Try Again!"
+        }
+
+if __name__ == '__main__':
+    app.run(debug=False)
