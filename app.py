4b0f89537759df950b768c59652c7ab4ce93c375
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-05-05 17:45:01 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,115 +1,157 @@
-import os
 import json
+from flask import Flask, render_template, request, jsonify
+import time
 import ccxt
-from flask import Flask, request, render_template
-from errors import errors_bp
+from custom_http import HTTP
 
 app = Flask(__name__)
-app.register_blueprint(errors_bp)
 
-# خواندن تنظیمات از فایل config.json
-with open('config.json', 'r') as config_file:
+# Load config.json
+with open('config.json') as config_file:
     config = json.load(config_file)
 
-# ایجاد اتصال به صرافی‌ها
-exchanges = {}
-if config['EXCHANGES']['binanceusdm']['ENABLED']:
-    exchange_config = {
-        'apiKey': config['EXCHANGES']['binanceusdm']['API_KEY'],
-        'secret': config['EXCHANGES']['binanceusdm']['API_SECRET'],
-        'options': {'defaultType': 'future'},
-        'enableRateLimit': True
-    }
-    if config['EXCHANGES']['binanceusdm']['TESTNET']:
-        exchange_config['urls'] = {
+def is_exchange_enabled(exchange_name):
+    return config['EXCHANGES'].get(exchange_name, {}).get('ENABLED', False)
+
+def create_order_binance(data, exchange):
+    symbol = data['symbol']
+    side = data['side']
+    price = data.get('price', 0)
+    quantity = data.get('quantity')
+
+    if quantity is None:
+        error_message = "The 'quantity' field is missing in the input data."
+        return {
+            "status": "error",
+            "message": error_message
+        }, 400
+
+    try:
+        order = exchange.create_order(
+            symbol=symbol,
+            type=data['type'],
+            side=side,
+            amount=float(quantity),
+            price=price
+        )
+        return {
+            "status": "success",
+            "data": order
+        }, 200
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }, 500
+
+def create_order_bybit(data, session):
+    symbol = data['symbol']
+    side = data['side']
+    price = data.get('price', 0)
+    quantity = data.get('quantity')
+
+    if quantity is None:
+        error_message = "The 'quantity' field is missing in the input data."
+        return {
+            "status": "error",
+            "message": error_message
+        }, 400
+
+    try:
+        order = session.post('/v2/private/order/create', json={
+            'symbol': symbol,
+            'side': side,
+            'order_type': data['type'],
+            'qty': float(quantity),
+            'price': price,
+            'time_in_force': 'GTC'
+        })
+        return {
+            "status": "success",
+            "data": order.json()
+        }, 200
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }, 500
+
+use_bybit = is_exchange_enabled('BYBIT')
+use_binance_futures = is_exchange_enabled('BINANCE-FUTURES')
+
+if use_bybit:
+    print("Bybit is enabled!")
+    session = HTTP(
+        endpoint='https://api.bybit.com',
+        api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
+        api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
+    )
+
+if use_binance_futures:
+    print("Binance is enabled!")
+    exchange = ccxt.binance({
+        'apiKey': config['EXCHANGES']['BINANCE-FUTURES']['API_KEY'],
+        'secret': config['EXCHANGES']['BINANCE-FUTURES']['API_SECRET'],
+        'options': {
+            'defaultType': 'future',
+        },
+        'urls': {
             'api': {
                 'public': 'https://testnet.binancefuture.com/fapi/v1',
                 'private': 'https://testnet.binancefuture.com/fapi/v1',
-                'fapiPublic': 'https://testnet.binancefuture.com/fapi/v1',
-                'fapiPrivate': 'https://testnet.binancefuture.com/fapi/v1'
-            }
+            },
         }
-    exchanges['binanceusdm'] = ccxt.binance(exchange_config)
-
-if config['EXCHANGES']['BYBIT']['ENABLED']:
-    exchanges['bybit'] = ccxt.bybit({
-        'apiKey': config['EXCHANGES']['BYBIT']['API_KEY'],
-        'secret': config['EXCHANGES']['BYBIT']['API_SECRET'],
-        'enableRateLimit': True
     })
 
-open_position = False
+    if config['EXCHANGES']['BINANCE-FUTURES']['TESTNET']:
+        exchange.set_sandbox_mode(True)
+
+@app.route('/')
+def index():
+    return {'message': 'Server is running!'}
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json
-    print("دریافت سیگنال:", data)
+    print("Hook Received!")
+    data = json.loads(request.data)
+    print(data)
 
-    exchange = data['exchange'].lower()
-    if exchange in exchanges:
-        execute_trade(exchanges[exchange], data)
-    else:
-        print(f"صرافی {exchange} فعال نیست.")
+    if int(data['key']) != config['KEY']:
+        error_message = "Invalid Key, Please Try Again!"
+        print(error_message)
+        return {
+            "status": "error",
+            "message": error_message
+        }, 400
 
-    return {
-        'status': 'success'
-    }, 200
-
-def execute_trade(exchange, data):
-    global open_position
-
-    symbol = data['symbol']
-    side = data['side']
-    if 'percentage' in data:
-        percentage = float(data['percentage'])
-    price = float(data['price'])
-
-    if side == 'closelong' or side == 'closeshort':
-        if open_position:
-            close_position(exchange, symbol, side)
-            open_position = False
+    if data['exchange'] == 'binance-futures':
+        if use_binance_futures:
+            response, status_code = create_order_binance(data, exchange)
+            return jsonify(response), status_code
         else:
-            print("هیچ معامله‌ای برای بستن وجود ندارد.")
-    else:
-        if not open_position:
-            if 'percentage' in data:
-                # دریافت موجودی کاربر
-                balance = exchange.fetch_balance()
+            error_message = "Binance Futures is not enabled in the config file."
+            return {
+                "status": "error",
+                "message": error_message
+            }, 400
 
-                # محاسبه مقدار معامله بر اساس درصد موجودی
-                base_currency = symbol.split('/')[0]
-                quote_currency = symbol.split('/')[1]
-                available_balance = balance[quote_currency]['free']
-                amount = (percentage / 100) * available_balance / price
-
-            # اجرای معامله
-            if side == 'long':
-                # خرید (long) در حالت فیوچرز
-                exchange.create_market_buy_order(symbol, amount)
-                open_position = True
-            elif side == 'short':
-                # فروش (short) در حالت فیوچرز
-                exchange.create_market_sell_order(symbol, amount)
-                open_position = True
-            else:
-                print("نوع معامله نامعتبر است.")
+    elif data['exchange'] == 'bybit':
+        if use_bybit:
+            response, status_code = create_order_bybit(data, session)
+            return jsonify(response), status_code
         else:
-            print("یک معامله باز است. لطفاً ابتدا معامله فعلی را ببندید.")
+            error_message = "Bybit is not enabled in the config file."
+            return {
+                "status": "error",
+                "message": error_message
+            }, 400
 
-def close_position(exchange, symbol, side):
-    if side == 'closelong':
-        position_side = 'long'
     else:
-        position_side = 'short'
-
-    if exchange == exchanges['binanceusdm']:
-        exchange.private_post_position_close({
-            'symbol': exchange.market_id(symbol),
-            'positionSide': position_side,
-            'reduceOnly': True
-        })
-    else:
-        print("بستن معامله در این صرافی پشتیبانی نمی‌شود.")
+        error_message = "Unsupported exchange."
+        return {
+            "status": "error",
+            "message": error_message
+        }, 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
