f1fb30c2bc262b64d62368b2a484221ec591f6dd
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-05-07 02:01:44 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import json
-from flask import Flask, render_template, request, jsonify
-import time
+from flask import Flask, jsonify, request
 import ccxt
 from custom_http import HTTP
 
 app = Flask(__name__)
 
-# load config.json
+# Load config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
 open_trade = False
 open_trade_id = None
 
+
 def is_exchange_enabled(exchange_name):
     if exchange_name in config['EXCHANGES']:
         if config['EXCHANGES'][exchange_name]['ENABLED']:
             return True
     return False
+
 
 def create_order_binance(data, exchange):
     symbol = data['symbol']
@@ -49,6 +50,7 @@ def create_order_binance(data, exchange):
             "status": "error",
             "message": str(e)
         }, 500
+
 
 def create_order_bybit(data, session):
     symbol = data['symbol']
@@ -101,6 +103,7 @@ def close_order_binance(order_id, symbol, side, remaining, exchange):
             "message": str(e)
         }, 500
 
+
 def close_order_bybit(order_id, symbol, side, remaining, session):
     try:
         order = session.post('/v2/private/order/create', json={
@@ -125,11 +128,11 @@ use_binance_futures = is_exchange_enabled('BINANCE-FUTURES')
 
 if use_bybit:
     print("Bybit is enabled!")
-session = HTTP(
-    endpoint='https://api.bybit.com',
-    api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
-    api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
-)
+    session = HTTP(
+        endpoint='https://api.bybit.com',
+        api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
+        api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
+    )
 
 if use_binance_futures:
     print("Binance is enabled!")
@@ -169,16 +172,23 @@ def webhook():
             "message": error_message
         }, 400
 
+    # Handle close trade requests
     if data.get('action') in ['closeshort', 'closelong']:
-        # Close trade
-        if data['exchange'] == 'binance-futures':
-            if use_binance_futures:
+        close_trade_handlers = {
+            'binance-futures': (use_binance_futures, close_order_binance),
+            'bybit': (use_bybit, close_order_bybit)
+        }
+
+        if data['exchange'] in close_trade_handlers:
+            enabled, handler = close_trade_handlers[data['exchange']]
+            if enabled:
                 if data['action'] == 'closeshort':
                     side = 'buy'
-                elif data['action'] == 'closelong':
+                else:
                     side = 'sell'
+
                 order = exchange.fetch_order(open_trade_id, symbol=data['symbol'])
-                response, status_code = close_order_binance(
+                response, status_code = handler(
                     open_trade_id,
                     data['symbol'],
                     side,
@@ -188,72 +198,38 @@ def webhook():
                 open_trade = False
                 open_trade_id = None
             else:
-                error_message = "Binance Futures is not enabled in the config file."
+                error_message = f"{data['exchange']} is not enabled in the config file."
                 return {
                     "status": "error",
                     "message": error_message
                 }, 400
-
-        elif data['exchange'] == 'bybit':
-            if use_bybit:
-                if data['action'] == 'closeshort':
-                    side = 'Buy'
-                elif data['action'] == 'closelong':
-                    side = 'Sell'
-                order = session.get(f'/v2/private/order?symbol={data["symbol"]}&order_id={open_trade_id}')
-                response, status_code = close_order_bybit(
-                    open_trade_id,
-                    data['symbol'],
-                    side,
-                    order.json()['result']['remaining'],
-                    session
-                )
-                open_trade = False
-                open_trade_id = None
-            else:
-                error_message = "Bybit is not enabled in the config file."
-                return {
-                    "status": "error",
-                    "message": error_message
-                }, 400
-
         else:
             error_message = "Unsupported exchange."
             return {
                 "status": "error",
                 "message": error_message
             }, 400
-
+    # Handle open trade requests
     elif not open_trade:
-        # Open trade
-        if data['exchange'] == 'binance-futures':
-            if use_binance_futures:
-                response, status_code = create_order_binance(data, exchange)
+        open_trade_handlers = {
+            'binance-futures': (use_binance_futures, create_order_binance),
+            'bybit': (use_bybit, create_order_bybit)
+        }
+
+        if data['exchange'] in open_trade_handlers:
+            enabled, handler = open_trade_handlers[data['exchange']]
+            if enabled:
+                response, status_code = handler(data, exchange if data['exchange'] == 'binance-futures' else session)
                 if status_code == 200:
                     open_trade = True
                     open_trade_id = response['data']['id']
                 return jsonify(response), status_code
             else:
-                error_message = "Binance Futures is not enabled in the config file."
+                error_message = f"{data['exchange']} is not enabled in the config file."
                 return {
                     "status": "error",
                     "message": error_message
                 }, 400
-
-        elif data['exchange'] == 'bybit':
-            if use_bybit:
-                response, status_code = create_order_bybit(data, session)
-                if status_code == 200:
-                    open_trade = True
-                    open_trade_id = response['data']['result']['order_id']
-                return jsonify(response), status_code
-            else:
-                error_message = "Bybit is not enabled in the config file."
-                return {
-                    "status": "error",
-                    "message": error_message
-                }, 400
-
         else:
             error_message = "Unsupported exchange."
             return {
