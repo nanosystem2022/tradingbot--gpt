450248665d635196b939e8dd91da660ba4a31196
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-05-08 14:22:49 +0330

    Update app.py

--- app.py
+++ app.py
@@ -19,18 +19,16 @@ def is_exchange_enabled(exchange_name):
 user_trade_status = {}
 
 def update_user_trade_status(data, response):
-    if response['status'] == 'error':
-        return
-    side = data['side']
     symbol = data['symbol']
-    if side.lower() == 'buy':
-        user_trade_status[symbol] = 'buy'
-    elif side.lower() == 'sell':
-        user_trade_status[symbol] = 'sell'
-    elif side.lower() == 'closelong':
-        user_trade_status[symbol] = None
-    elif side.lower() == 'closeshort':
-        user_trade_status[symbol] = None
+    side = data['side']
+
+    if response['status'] == 'success':
+        if side == 'closelong':
+            user_trade_status[symbol] = 'sell'
+        elif side == 'closeshort':
+            user_trade_status[symbol] = 'buy'
+        else:
+            user_trade_status[symbol] = side
 
 def create_order_binance(data, exchange):
     symbol = data['symbol']
@@ -153,7 +151,7 @@ def webhook():
             return {"status": "error", "message": "Cannot close long because there is no open long position."}, 400
         if side == 'closeshort' and user_trade_status[symbol] != 'sell':
             return {"status": "error", "message": "Cannot close short because there is no open short position."}, 400
-
+ 
     if data['exchange'] == 'binance-futures':
         if use_binance_futures:
             response, status_code = create_order_binance(data, exchange)
