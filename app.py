f339e3e2d8de2d1385e9c9466a73abbd91b4532d
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-05-09 18:25:55 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json
-import http
 from flask import Flask, render_template, request, jsonify
 import time
 import ccxt
@@ -7,19 +6,11 @@ from custom_http import HTTP
 
 app = Flask(__name__)
 
-# Load config.json
+# load config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-# Add a variable to track open orders
-open_orders = {}
-
-# Error messages
-ERR_UNSUPPORTED_ACTION = {"status": "error", "message": "Unsupported action."}
-ERR_UNSUPPORTED_EXCHANGE = {"status": "error", "message": "Unsupported exchange."}
-ERR_MISSING_QUANTITY = {"status": "error", "message": "The 'quantity' field is missing in the input data."}
-ERR_NO_OPEN_ORDER = {"status": "error", "message": "No open order found."}
-
+current_position = 'closed'
 
 def is_exchange_enabled(exchange_name):
     if exchange_name in config['EXCHANGES']:
@@ -27,37 +18,36 @@ def is_exchange_enabled(exchange_name):
             return True
     return False
 
-def create_order_binance(data):
-    symbol = data['symbol'].replace('/', '')
-    order_type = data.get('type', 'market')
-    side = data.get('side', None)
-    quantity = data.get('quantity', None)
+def create_order_binance(data, exchange):
+    symbol = data['symbol']
+    side = data['side']
+    price = data.get('price', 0)
+    quantity = data.get('quantity')
 
-    if data['action'] == 'closelong':
-        side = 'sell'
-    elif data['action'] == 'closeshort':
-        side = 'buy'
-
-    if side is None or quantity is None:
-        return {'error': 'Missing side or quantity'}, 400
+    if quantity is None:
+        error_message = "The 'quantity' field is missing in the input data."
+        return {
+            "status": "error",
+            "message": error_message
+        }, 400
 
     try:
-        if data['action'] == 'open':
-            order = exchange.create_order(symbol, order_type, side, quantity)
-        elif data['action'] == 'closelong' or data['action'] == 'closeshort':
-            position = exchange.fetch_positions({'symbol': symbol})
-            if position:
-                quantity_to_close = position[0]['contracts'] if data['action'] == 'closelong' else position[1]['contracts']
-                order = exchange.create_order(symbol, order_type, side, quantity_to_close)
-            else:
-                return {'error': 'No open position found'}, 400
-        else:
-            return {'error': 'Invalid action'}, 400
-
-        return {'result': order}, 200
+        order = exchange.create_order(
+            symbol=symbol,
+            type=data['type'],
+            side=side,
+            amount=float(quantity),
+            price=price
+        )
+        return {
+            "status": "success",
+            "data": order
+        }, 200
     except Exception as e:
-        return {'error': str(e)}, 400
-
+        return {
+            "status": "error",
+            "message": str(e)
+        }, 500
 
 def create_order_bybit(data, session):
     symbol = data['symbol']
@@ -90,40 +80,17 @@ def create_order_bybit(data, session):
             "status": "error",
             "message": str(e)
         }, 500
-    
-def close_order_binance(data, exchange):
-    order_id = open_orders.get(data['symbol'])
-    if order_id:
-        try:
-            order = exchange.cancel_order(order_id, symbol=data['symbol'])
-            return {"status": "success", "data": order}, 200
-        except Exception as e:
-            return {"status": "error", "message": str(e)}, 500
-    else:
-        return {"status": "error", "message": "No open order found."}, 400
-
-def close_order_bybit(data, session):
-    order_id = open_orders.get(data['symbol'])
-    if order_id:
-        try:
-            order = session.post('/v2/private/order/cancel', json={'order_id': order_id})
-            return {"status": "success", "data": order.json()}, 200
-        except Exception as e:
-            return {"status": "error", "message": str(e)}, 500
-    else:
-        return {"status": "error", "message": "No open order found."}, 400
-
 
 use_bybit = is_exchange_enabled('BYBIT')
 use_binance_futures = is_exchange_enabled('BINANCE-FUTURES')
 
 if use_bybit:
     print("Bybit is enabled!")
-session = HTTP(
-    endpoint='https://api.bybit.com',
-    api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
-    api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
-)
+    session = HTTP(
+        endpoint='https://api.bybit.com',
+        api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
+        api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
+    )
 
 if use_binance_futures:
     print("Binance is enabled!")
@@ -138,6 +105,7 @@ if use_binance_futures:
                 'public': 'https://testnet.binancefuture.com/fapi/v1',
                 'private': 'https://testnet.binancefuture.com/fapi/v1',
             },
+       
         }
     })
 
@@ -150,31 +118,58 @@ def index():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    global current_position
     print("Hook Received!")
-    data = request.json
+    data = json.loads(request.data)
     print(data)
 
-    actions = {
-        'binance-futures': {
-            'open': create_order_binance,
-            'close': close_order_binance
-        },
-        'bybit': {
-            'open': create_order_bybit,
-            'close': close_order_bybit
-        }
-    }
+    if int(data['key']) != config['KEY']:
+        error_message = "Invalid Key, Please Try Again!"
+        print(error_message)
+        return {
+            "status": "error",
+            "message": error_message
+        }, 400
 
-    exchange_actions = actions.get(data['exchange'])
-    if exchange_actions:
-        action = exchange_actions.get(data['action'])
-        if action:
-            response, status_code = action(data, exchange if data['exchange'] == 'binance-futures' else session)
-            return jsonify(response), status_code
+    if current_position == 'closed' or data['side'] == 'closelong' or data['side'] == 'closeshort':
+        if data['exchange'] == 'binance-futures':
+            if use_binance_futures:
+                response, status_code = create_order_binance(data, exchange)
+                if data['side'] == 'long' or data['side'] == 'short':
+                    current_position = data['side']
+                return jsonify(response), status_code
+            else:
+                error_message = "Binance Futures is not enabled in the config file."
+                return {
+                    "status": "error",
+                    "message": error_message
+                }, 400
+
+        elif data['exchange'] == 'bybit':
+            if use_bybit:
+                response, status_code = create_order_bybit(data, session)
+                if data['side'] == 'long' or data['side'] == 'short':
+                    current_position = data['side']
+                return jsonify(response), status_code
+            else:
+                error_message = "Bybit is not enabled in the config file."
+                return {
+                    "status": "error",
+                    "message": error_message
+                }, 400
+
         else:
-            return jsonify(ERR_UNSUPPORTED_ACTION), http.HTTPStatus.BAD_REQUEST
+            error_message = "Unsupported exchange."
+            return {
+                "status": "error",
+                "message": error_message
+            }, 400
     else:
-        return jsonify(ERR_UNSUPPORTED_EXCHANGE), http.HTTPStatus.BAD_REQUEST
+        error_message = "Cannot accept new orders until current position is closed."
+        return {
+            "status": "error",
+            "message": error_message
+        }, 400
 
 if __name__ == '__main__':
     app.run()
