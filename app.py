0a6763c423cd35c6a3bc35d7b65bb21f8359b6b9
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-07-08 21:00:05 +0330

    Update app.py

--- app.py
+++ app.py
@@ -14,35 +14,28 @@ with open('config.json') as config_file:
 current_position = 'closed'
 
 def is_exchange_enabled(exchange_name):
-    if exchange_name in config['EXCHANGES']:
-        if config['EXCHANGES'][exchange_name]['ENABLED']:
-            return True
-    return False
+    return exchange_name in config['EXCHANGES'] and config['EXCHANGES'][exchange_name]['ENABLED']
 
 def create_order_binance(data, exchange):
-    try:
-        symbol = data['symbol']
-        order_type = data['type']
-        side = data['side']
-        quantity = data['quantity']
+    symbol = data['symbol']
+    order_type = data['type']
+    side = data['side']
+    quantity = data['quantity']
 
-        if side == "closelong":
-            side = "sell"
-        elif side == "closeshort":
-            side = "buy"
+    if side == "closelong":
+        side = "sell"
+    elif side == "closeshort":
+        side = "buy"
 
-        if order_type == "market":
-            order = exchange.create_market_order(symbol, side, quantity)
-        elif order_type == "limit":
-            price = data['price']
-            order = exchange.create_limit_order(symbol, side, quantity, price)
-        else:
-            return {"status": "error", "message": "Invalid order type"}, 400
+    if order_type == "market":
+        order = exchange.create_market_order(symbol, side, quantity)
+    elif order_type == "limit":
+        price = data['price']
+        order = exchange.create_limit_order(symbol, side, quantity, price)
+    else:
+        raise ValueError("Invalid order type")
 
-        return {"status": "success", "data": order}, 200
-    except Exception as e:
-        return {"status": "error", "message": f"Error creating order: {str(e)}"}, 500
-
+    return order
 
 def create_order_bybit(data, session):
     symbol = data['symbol']
@@ -51,30 +44,17 @@ def create_order_bybit(data, session):
     quantity = data.get('quantity')
 
     if quantity is None:
-        error_message = "The 'quantity' field is missing in the input data."
-        return {
-            "status": "error",
-            "message": error_message
-        }, 400
+        raise ValueError("The 'quantity' field is missing in the input data.")
 
-    try:
-        order = session.post('/v2/private/order/create', json={
-            'symbol': symbol,
-            'side': side,
-            'order_type': data['type'],
-            'qty': float(quantity),
-            'price': price,
-            'time_in_force': 'GTC'
-        })
-        return {
-            "status": "success",
-            "data": order.json()
-        }, 200
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }, 500
+    order = session.post('/v2/private/order/create', json={
+        'symbol': symbol,
+        'side': side,
+        'order_type': data['type'],
+        'qty': float(quantity),
+        'price': price,
+        'time_in_force': 'GTC'
+    })
+    return order.json()
 
 def close_order_binance(data, exchange):
     symbol = data['symbol']
@@ -83,36 +63,19 @@ def close_order_binance(data, exchange):
     quantity = data.get('quantity')
 
     if side not in ['closelong', 'closeshort']:
-        error_message = "Invalid side value for closing order. Use 'closelong' or 'closeshort'."
-        return {
-            "status": "error",
-            "message": error_message
-        }, 400
+        raise ValueError("Invalid side value for closing order. Use 'closelong' or 'closeshort'.")
 
     if quantity is None:
-        error_message = "The 'quantity' field is missing in the input data."
-        return {
-            "status": "error",
-            "message": error_message
-        }, 400
+        raise ValueError("The 'quantity' field is missing in the input data.")
 
-    try:
-        order = exchange.create_order(
-            symbol=symbol,
-            type=data['type'],
-            side='sell' if side == 'closelong' else 'buy',
-            amount=float(quantity),
-            price=price
-        )
-        return {
-            "status": "success",
-            "data": order
-        }, 200
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }, 500
+    order = exchange.create_order(
+        symbol=symbol,
+        type=data['type'],
+        side='sell' if side == 'closelong' else 'buy',
+        amount=float(quantity),
+        price=price
+    )
+    return order
 
 def close_order_bybit(data, session):
     symbol = data['symbol']
@@ -121,37 +84,20 @@ def close_order_bybit(data, session):
     quantity = data.get('quantity')
 
     if side not in ['closelong', 'closeshort']:
-        error_message = "Invalid side value for closing order. Use 'closelong' or 'closeshort'."
-        return {
-            "status": "error",
-            "message": error_message
-        }, 400
+        raise ValueError("Invalid side value for closing order. Use 'closelong' or 'closeshort'.")
 
     if quantity is None:
-        error_message = "The 'quantity' field is missing in the input data."
-        return {
-            "status": "error",
-            "message": error_message
-        }, 400
+        raise ValueError("The 'quantity' field is missing in the input data.")
 
-    try:
-        order = session.post('/v2/private/order/create', json={
-            'symbol': symbol,
-            'side': 'sell' if side == 'closelong' else 'buy',
-            'order_type': data['type'],
-            'qty': float(quantity),
-            'price': price,
-            'time_in_force': 'GTC'
-        })
-        return {
-            "status": "success",
-            "data": order.json()
-        }, 200
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }, 500
+    order = session.post('/v2/private/order/create', json={
+        'symbol': symbol,
+        'side': 'sell' if side == 'closelong' else 'buy',
+        'order_type': data['type'],
+        'qty': float(quantity),
+        'price': price,
+        'time_in_force': 'GTC'
+    })
+    return order.json()
 
 use_bybit = is_exchange_enabled('BYBIT')
 use_binance_futures = is_exchange_enabled('BINANCE-FUTURES')
@@ -205,41 +151,35 @@ def webhook():
 
     close_order = data['side'] == 'closelong' or data['side'] == 'closeshort'
     if current_position == 'closed' or close_order:
-        if data['exchange'] == 'binance-futures':
-            if use_binance_futures:
-                response, status_code = create_order_binance(data, exchange)
-                if data['side'] == 'long' or data['side'] == 'short':
-                    current_position = data['side']
-                return jsonify(response), status_code
+        try:
+            if data['exchange'] == 'binance-futures':
+                if use_binance_futures:
+                    response = create_order_binance(data, exchange)
+                    if data['side'] == 'long' or data['side'] == 'short':
+                        current_position = data['side']
+                    return {"status": "success", "data": response}, 200
+                else:
+                    raise ValueError("Binance Futures is not enabled in the config file.")
+
+            elif data['exchange'] == 'bybit':
+                if use_bybit:
+                    response = create_order_bybit(data, session)
+                    if data['side'] == 'long' or data['side'] == 'short':
+                        current_position = data['side']
+                    return {"status": "success", "data": response}, 200
+                else:
+                    raise ValueError("Bybit is not enabled in the config file.")
+
             else:
-                error_message = "Binance Futures is not enabled in the config file."
-                return {
-                    "status": "error",
-                    "message": error_message
-                }, 400
+                raise ValueError("Unsupported exchange.")
 
-        elif data['exchange'] == 'bybit':
-            if use_bybit:
-                response, status_code = create_order_bybit(data, session)
-                if data['side'] == 'long' or data['side'] == 'short':
-                    current_position = data['side']
-                return jsonify(response), status_code
-            else:
-                error_message = "Bybit is not enabled in the config file."
-                return {
-                    "status": "error",
-                    "message": error_message
-                }, 400
+            if close_order:
+                current_position = 'closed'
 
-        else:
-            error_message = "Unsupported exchange."
-            return {
-                "status": "error",
-                "message": error_message
-            }, 400
-
-        if close_order:
-            current_position = 'closed'
+        except ValueError as e:
+            return {"status": "error", "message": str(e)}, 400
+        except Exception as e:
+            return {"status": "error", "message": str(e)}, 500
 
     else:
         error_message = "Cannot accept new orders until current position is closed."
