25a14fc0c45dec174d538004db4ac01e4515eea6
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-07-08 17:45:32 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import json
-from flask import Flask, render_template, request, jsonify
-import time
+from flask import Flask, request, jsonify
 import ccxt
 from custom_http import HTTP
 
@@ -23,6 +22,7 @@ def create_order_binance(data, exchange):
     side = data['side']
     price = data.get('price', 0)
     quantity = data.get('quantity')
+    params = {'closePosition': 'Close-All'} if side == 'closeshort' or side == 'closelong' else {}
 
     if quantity is None:
         error_message = "The 'quantity' field is missing in the input data."
@@ -37,7 +37,8 @@ def create_order_binance(data, exchange):
             type=data['type'],
             side=side,
             amount=float(quantity),
-            price=price
+            price=price,
+            params=params
         )
         return {
             "status": "success",
@@ -54,6 +55,7 @@ def create_order_bybit(data, session):
     side = data['side']
     price = data.get('price', 0)
     quantity = data.get('quantity')
+    params = {'reduce_only': True} if side == 'closeshort' or side == 'closelong' else {}
 
     if quantity is None:
         error_message = "The 'quantity' field is missing in the input data."
@@ -69,40 +71,12 @@ def create_order_bybit(data, session):
             'order_type': data['type'],
             'qty': float(quantity),
             'price': price,
-            'time_in_force': 'GTC'
+            'time_in_force': 'GTC',
+            **params
         })
         return {
             "status": "success",
             "data": order.json()
-        }, 200
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }, 500
-
-def close_order_bybit(symbol, order_id, session):
-    try:
-        res = session.post('/v2/private/order/cancel', json={
-            'symbol': symbol,
-            'order_id': order_id
-        })
-        return {
-            "status": "success",
-            "data": res.json()
-        }, 200
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }, 500
-
-def close_order_binance(symbol, order_id, exchange):
-    try:
-        res = exchange.cancel_order(order_id, symbol)
-        return {
-            "status": "success",
-            "data": res
         }, 200
     except Exception as e:
         return {
@@ -121,37 +95,6 @@ if use_binance:
         'secret': config['EXCHANGES']['binance']['SECRET']
     })
 
-def create_order(data):
-    if use_bybit:
-        response, status = create_order_bybit(data, session_bybit)
-        if status == 200:
-            active_orders[data['symbol']] = response['data']['result']['order_id']
-    if use_binance:
-        response, status = create_order_binance(data, binance)
-        if status == 200:
-            active_orders[data['symbol']] = response['data']['id']
-
-    return jsonify(response), status
-
-def close_order(data):
-    symbol = data['symbol']
-
-    if symbol not in active_orders:
-        return jsonify({
-            "status": "error",
-            "message": "There's no active order for this symbol"
-        }), 400
-
-    if use_bybit:
-        response, status = close_order_bybit(symbol, active_orders[symbol], session_bybit)
-    if use_binance:
-        response, status = close_order_binance(symbol, active_orders[symbol], binance)
-
-    if status == 200:
-        del active_orders[symbol]
-
-    return jsonify(response), status
-
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
     data = request.json
@@ -164,9 +107,25 @@ def handle_webhook():
         }), 400
 
     if side in ['buy', 'sell']:
-        return create_order(data)
+        if data['exchange'] == 'binance':
+            return create_order_binance(data, binance)
+        elif data['exchange'] == 'bybit':
+            return create_order_bybit(data, session_bybit)
+        else:
+            return jsonify({
+                "status": "error",
+                "message": "Unsupported exchange."
+            }), 400
     elif side in ['closeshort', 'closelong']:
-        return close_order(data)
+        if data['exchange'] == 'binance':
+            return create_order_binance(data, binance)
+        elif data['exchange'] == 'bybit':
+            return create_order_bybit(data, session_bybit)
+        else:
+            return jsonify({
+                "status": "error",
+                "message": "Unsupported exchange."
+            }), 400
     else:
         return jsonify({
             "status": "error",
