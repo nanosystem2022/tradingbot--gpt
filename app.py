179c18358f59fb180bc7cd913864f1a21827f91e
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-07-08 20:05:17 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
+import time
 import ccxt
 from custom_http import HTTP
 
@@ -10,6 +11,7 @@ with open('config.json') as config_file:
     config = json.load(config_file)
 
 active_orders = {} # to store the active orders
+trade_open = False # to store the status of the trade
 
 def is_exchange_enabled(exchange_name):
     if exchange_name in config['EXCHANGES']:
@@ -95,6 +97,49 @@ if use_binance:
         'secret': config['EXCHANGES']['binance']['SECRET']
     })
 
+def create_order(data):
+    global trade_open
+    if trade_open:
+        return jsonify({
+            "status": "error",
+            "message": "A trade is already open. Please close the current trade before opening a new one."
+        }), 400
+
+    if use_bybit:
+        response, status = create_order_bybit(data, session_bybit)
+        if status == 200:
+            active_orders[data['symbol']] = response['data']['result']['order_id']
+    if use_binance:
+        response, status = create_order_binance(data, binance)
+        if status == 200:
+            active_orders[data['symbol']] = response['data']['id']
+
+    if status == 200:
+        trade_open = True
+
+    return jsonify(response), status
+
+def close_order(data):
+    global trade_open
+    symbol = data['symbol']
+
+    if symbol not in active_orders:
+        return jsonify({
+            "status": "error",
+            "message": "There's no active order for this symbol"
+        }), 400
+
+    if use_bybit:
+        response, status = close_order_bybit(symbol, active_orders[symbol], session_bybit)
+    if use_binance:
+        response, status = close_order_binance(symbol, active_orders[symbol], binance)
+
+    if status == 200:
+        del active_orders[symbol]
+        trade_open = False
+
+    return jsonify(response), status
+
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
     data = request.json
@@ -107,25 +152,9 @@ def handle_webhook():
         }), 400
 
     if side in ['buy', 'sell']:
-        if data['exchange'] == 'binance':
-            return create_order_binance(data, binance)
-        elif data['exchange'] == 'bybit':
-            return create_order_bybit(data, session_bybit)
-        else:
-            return jsonify({
-                "status": "error",
-                "message": "Unsupported exchange."
-            }), 400
+        return create_order(data)
     elif side in ['closeshort', 'closelong']:
-        if data['exchange'] == 'binance':
-            return create_order_binance(data, binance)
-        elif data['exchange'] == 'bybit':
-            return create_order_bybit(data, session_bybit)
-        else:
-            return jsonify({
-                "status": "error",
-                "message": "Unsupported exchange."
-            }), 400
+        return close_order(data)
     else:
         return jsonify({
             "status": "error",
