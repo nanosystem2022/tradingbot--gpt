e8b93208ff9c814efc91af75d3b68562987b8746
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-07-09 02:15:24 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
-import json
 import os
-from flask import Flask, render_template, request, jsonify
+import json
+import hmac
 import time
+from flask import Flask, request
 import ccxt
-from custom_http import HTTP
+from requests import Session
 
 app = Flask(__name__)
 
-# load config.json
+# Load config.json
 with open('config.json') as config_file:
     config = json.load(config_file)
 
 current_position = 'closed'
 current_side = None
 
+class HTTP(Session):
+    def __init__(self, endpoint, api_key, api_secret):
+        super().__init__()
+        self.endpoint = endpoint
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def request(self, method, path, *args, **kwargs):
+        url = self.endpoint + path
+
+        # Sign the request if needed
+        if self.api_key and self.api_secret:
+            timestamp = int(time.time() * 1000)
+            kwargs['headers'] = kwargs.get('headers', {})
+            kwargs['headers'].update({
+                'api-key': self.api_key,
+                'api-expires': str(timestamp + 5000),
+            })
+
+            # Create signature
+            signature_payload = f"{method}\n{path}\n{timestamp}"
+            signature = hmac.new(
+                self.api_secret.encode('utf-8'),
+                signature_payload.encode('utf-8'),
+                digestmod='sha256'
+            ).hexdigest()
+
+            kwargs['headers']['api-signature'] = signature
+
+        return super().request(method, url, *args, **kwargs)
+
 def is_exchange_enabled(exchange_name):
     return exchange_name in config['EXCHANGES'] and config['EXCHANGES'][exchange_name]['ENABLED']
 
-def create_order_binance(data, exchange):
+def create_order(data, session, exchange=None):
     symbol = data['symbol']
     order_type = data['type']
     side = data['side']
@@ -29,35 +61,30 @@ def create_order_binance(data, exchange):
         side = "buy"
 
     if order_type == "market":
-        order = exchange.create_market_order(symbol, side, quantity)
+        order = exchange.create_market_order(symbol, side, quantity) if exchange else session.post('/v2/private/order/create', json={
+            'symbol': symbol,
+            'side': side,
+            'order_type': data['type'],
+            'qty': float(quantity),
+            'price': 0,
+            'time_in_force': 'GTC'
+        }).json()
     elif order_type == "limit":
         price = data['price']
-        order = exchange.create_limit_order(symbol, side, quantity, price)
+        order = exchange.create_limit_order(symbol, side, quantity, price) if exchange else session.post('/v2/private/order/create', json={
+            'symbol': symbol,
+            'side': side,
+            'order_type': data['type'],
+            'qty': float(quantity),
+            'price': price,
+            'time_in_force': 'GTC'
+        }).json()
     else:
         raise ValueError("Invalid order type")
 
     return order
 
-def create_order_bybit(data, session):
-    symbol = data['symbol']
-    side = data['side']
-    price = data.get('price', 0)
-    quantity = data.get('quantity')
-
-    if quantity is None:
-        raise ValueError("The 'quantity' field is missing in the input data.")
-
-    order = session.post('/v2/private/order/create', json={
-        'symbol': symbol,
-        'side': side,
-        'order_type': data['type'],
-        'qty': float(quantity),
-        'price': price,
-        'time_in_force': 'GTC'
-    })
-    return order.json()
-
-def close_order_binance(data, exchange):
+def close_order(data, session, exchange=None):
     symbol = data['symbol']
     side = data['side']
     price = data.get('price', 0)
@@ -75,33 +102,22 @@ def close_order_binance(data, exchange):
         side='sell' if side == 'closelong' else 'buy',
         amount=float(quantity),
         price=price
-    )
-    return order
-
-def close_order_bybit(data, session):
-    symbol = data['symbol']
-    side = data['side']
-    price = data.get('price', 0)
-    quantity = data.get('quantity')
-
-    if side not in ['closelong', 'closeshort']:
-        raise ValueError("Invalid side value for closing order. Use 'closelong' or 'closeshort'.")
-
-    if quantity is None:
-        raise ValueError("The 'quantity' field is missing in the input data.")
-
-    order = session.post('/v2/private/order/create', json={
+    ) if exchange else session.post('/v2/private/order/create', json={
         'symbol': symbol,
         'side': 'sell' if side == 'closelong' else 'buy',
         'order_type': data['type'],
         'qty': float(quantity),
         'price': price,
         'time_in_force': 'GTC'
-    })
-    return order.json()
+    }).json()
+
+    return order
 
 use_bybit = is_exchange_enabled('BYBIT')
 use_binance_futures = is_exchange_enabled('BINANCE-FUTURES')
+
+session = None
+exchange = None
 
 if use_bybit:
     print("Bybit is enabled!")
@@ -151,14 +167,14 @@ def webhook():
             if use_binance_futures:
                 if data['side'] in ['buy', 'sell']:
                     if current_position == 'closed':
-                        response = create_order_binance(data, exchange)
+                        response = create_order(data, None, exchange)
                         current_position = 'open'
                         current_side = data['side']
                     else:
                         raise ValueError("Cannot open a new order until the current one is closed.")
                 elif data['side'] in ['closelong', 'closeshort']:
                     if current_position == 'open' and ((current_side == 'buy' and data['side'] == 'closelong') or (current_side == 'sell' and data['side'] == 'closeshort')):
-                        response = close_order_binance(data, exchange)
+                        response = close_order(data, None, exchange)
                         current_position = 'closed'
                     else:
                         raise ValueError("Cannot close the order. Either there is no open order or the side of the closing order does not match the side of the open order.")
@@ -172,14 +188,14 @@ def webhook():
             if use_bybit:
                 if data['side'] in ['buy', 'sell']:
                     if current_position == 'closed':
-                        response = create_order_bybit(data, session)
+                        response = close_order_bybit(data, session)
                         current_position = 'open'
                         current_side = data['side']
                     else:
                         raise ValueError("Cannot open a new order until the current one is closed.")
                 elif data['side'] in ['closelong', 'closeshort']:
                     if current_position == 'open' and ((current_side == 'buy' and data['side'] == 'closelong') or (current_side == 'sell' and data['side'] == 'closeshort')):
-                        response = close_order_bybit(data, session)
+                        response = close_order(data, session)
                         current_position = 'closed'
                     else:
                         raise ValueError("Cannot close the order. Either there is no open order or the side of the closing order does not match the side of the open order.")
