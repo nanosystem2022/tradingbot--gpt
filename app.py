cc28637e99e7df82646cf3d6501c3e3177851030
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-07-23 13:54:40 +0330

    Update app.py

--- app.py
+++ app.py
@@ -38,22 +38,6 @@ def create_order_binance(data, exchange):
 
     return order
 
-def create_spot_order_binance(data, exchange):
-    symbol = data['symbol']
-    order_type = data['type']
-    side = data['side']
-    quantity = data['quantity']
-
-    if order_type == "market":
-        order = exchange.create_market_order(symbol, side, quantity)
-    elif order_type == "limit":
-        price = data['price']
-        order = exchange.create_limit_order(symbol, side, quantity, price)
-    else:
-        raise ValueError("Invalid order type")
-
-    return order
-
 def create_order_bybit(data, session):
     symbol = data['symbol']
     side = data['side']
@@ -118,7 +102,6 @@ def close_order_bybit(data, session):
 
 use_bybit = is_exchange_enabled('BYBIT')
 use_binance_futures = is_exchange_enabled('BINANCE-FUTURES')
-use_binance_spot = is_exchange_enabled('BINANCE-SPOT')
 
 if use_bybit:
     print("Bybit is enabled!")
@@ -141,21 +124,12 @@ if use_binance_futures:
                 'public': 'https://testnet.binancefuture.com/fapi/v1',
                 'private': 'https://testnet.binancefuture.com/fapi/v1',
             },
+       
         }
     })
 
     if config['EXCHANGES']['BINANCE-FUTURES']['TESTNET']:
         exchange.set_sandbox_mode(True)
-
-if use_binance_spot:
-    print("Binance Spot is enabled!")
-    exchange_spot = ccxt.binance({
-        'apiKey': config['EXCHANGES']['BINANCE-SPOT']['API_KEY'],
-        'secret': config['EXCHANGES']['BINANCE-SPOT']['API_SECRET'],
-        'options': {
-            'defaultType': 'spot',
-        }
-    })
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -193,21 +167,6 @@ def webhook():
                 return {"status": "success", "data": response}, 200
             else:
                 raise ValueError("Binance Futures is not enabled in the config file.")
-
-        elif data['exchange'] == 'binance-spot':
-            if use_binance_spot:
-                if data['side'] in ['buy', 'sell']:
-                    if current_position == 'closed':
-                        response = create_spot_order_binance(data, exchange_spot)
-                        current_position = 'open'
-                        current_side = data['side']
-                    else:
-                        raise ValueError("Cannot open a new order until the current one is closed.")
-                else:
-                    raise ValueError("Invalid side value. Use 'buy' or 'sell'.")
-                return {"status": "success", "data": response}, 200
-            else:
-                raise ValueError("Binance Spot is not enabled in the config file.")
 
         elif data['exchange'] == 'bybit':
             if use_bybit:
