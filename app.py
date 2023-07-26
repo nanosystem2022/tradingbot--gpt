13dfb523f508d424f6c9161e8d7042a28f1b5a1e
nanosystem2022 <104572032+nanosystem2022@users.noreply.github.com>
2023-07-26 19:45:00 +0330

    Update app.py

--- app.py
+++ app.py
@@ -11,77 +11,109 @@ app = Flask(__name__)
 with open('config.json') as config_file:
     config = json.load(config_file)
 
-class OrderManager:
-    def __init__(self):
-        self.current_position = 'closed'
-        self.current_side = None
+current_position = 'closed'
+current_side = None
 
-    def can_open_order(self):
-        return self.current_position == 'closed'
+def is_exchange_enabled(exchange_name):
+    return exchange_name in config['EXCHANGES'] and config['EXCHANGES'][exchange_name]['ENABLED']
 
-    def can_close_order(self, side):
-        return self.current_position == 'open' and ((self.current_side == 'buy' and side == 'closelong') or (self.current_side == 'sell' and side == 'closeshort'))
+def create_order(data, exchange):
+    symbol = data['symbol']
+    order_type = data['type']
+    side = data['side']
+    balance = exchange.fetch_balance()
+    free_usdt = balance['USDT']['free']
+    quantity = free_usdt * 0.5  # 50% of free balance
 
-order_manager = OrderManager()
+    if side == "closelong":
+        side = "sell"
+    elif side == "closeshort":
+        side = "buy"
 
-class ErrorHandler:
-    @staticmethod
-    def handle_error(e):
-        return {"status": "error", "message": str(e)}, 400 if isinstance(e, ValueError) else 500
+    if order_type == "market":
+        order = exchange.create_market_order(symbol, side, quantity)
+    elif order_type == "limit":
+        price = data['price']
+        order = exchange.create_limit_order(symbol, side, quantity, price)
+    else:
+        raise ValueError("Invalid order type")
 
-error_handler = ErrorHandler()
+    return order
 
-class ExchangeManager:
-    def __init__(self, config):
-        self.config = config
-        self.use_bybit = self.is_exchange_enabled('BYBIT')
-        self.use_binance_futures = self.is_exchange_enabled('BINANCE-FUTURES')
-        self.use_binance_spot = self.is_exchange_enabled('BINANCE-SPOT')
+def close_order(data, exchange):
+    symbol = data['symbol']
+    side = data['side']
+    price = data.get('price', 0)
+    quantity = data.get('quantity')
 
-        if self.use_bybit:
-            print("Bybit is enabled!")
-            self.session = HTTP(
-                endpoint='https://api.bybit.com',
-                api_key=self.config['EXCHANGES']['BYBIT']['API_KEY'],
-                api_secret=self.config['EXCHANGES']['BYBIT']['API_SECRET']
-            )
+    if side not in ['closelong', 'closeshort']:
+        raise ValueError("Invalid side value for closing order. Use 'closelong' or 'closeshort'.")
 
-        if self.use_binance_futures:
-            print("Binance is enabled!")
-            self.exchange = ccxt.binance({
-                'apiKey': self.config['EXCHANGES']['BINANCE-FUTURES']['API_KEY'],
-                'secret': self.config['EXCHANGES']['BINANCE-FUTURES']['API_SECRET'],
-                'options': {
-                    'defaultType': 'future',
-                },
-                'urls': {
-                    'api': {
-                        'public': 'https://testnet.binancefuture.com/fapi/v1',
-                        'private': 'https://testnet.binancefuture.com/fapi/v1',
-                    },
-                }
-            })
+    if quantity is None:
+        raise ValueError("The 'quantity' field is missing in the input data.")
 
-            if self.config['EXCHANGES']['BINANCE-FUTURES']['TESTNET']:
-                self.exchange.set_sandbox_mode(True)
+    order = exchange.create_order(
+        symbol=symbol,
+        type=data['type'],
+        side='sell' if side == 'closelong' else 'buy',
+        amount=float(quantity),
+        price=price
+    )
+    return order
 
-        if self.use_binance_spot:
-            print("Binance Spot is enabled!")
-            self.exchange_spot = ccxt.binance({
-                'apiKey': self.config['EXCHANGES']['BINANCE-SPOT']['API_KEY'],
-                'secret': self.config['EXCHANGES']['BINANCE-SPOT']['API_SECRET'],
-                'options': {
-                    'defaultType': 'spot',
-                }
-            })
+def handle_error(e):
+    return {"status": "error", "message": str(e)}, 400 if isinstance(e, ValueError) else 500
 
-    def is_exchange_enabled(self, exchange_name):
-        return exchange_name in self.config['EXCHANGES'] and self.config['EXCHANGES'][exchange_name]['ENABLED']
+def can_open_order(current_position):
+    return current_position == 'closed'
 
-exchange_manager = ExchangeManager(config)
+def can_close_order(current_position, current_side, side):
+    return current_position == 'open' and ((current_side == 'buy' and side == 'closelong') or (current_side == 'sell' and side == 'closeshort'))
+
+use_bybit = is_exchange_enabled('BYBIT')
+use_binance_futures = is_exchange_enabled('BINANCE-FUTURES')
+use_binance_spot = is_exchange_enabled('BINANCE-SPOT')
+
+if use_bybit:
+    print("Bybit is enabled!")
+    session = HTTP(
+        endpoint='https://api.bybit.com',
+        api_key=config['EXCHANGES']['BYBIT']['API_KEY'],
+        api_secret=config['EXCHANGES']['BYBIT']['API_SECRET']
+    )
+
+if use_binance_futures:
+    print("Binance is enabled!")
+    exchange = ccxt.binance({
+        'apiKey': config['EXCHANGES']['BINANCE-FUTURES']['API_KEY'],
+        'secret': config['EXCHANGES']['BINANCE-FUTURES']['API_SECRET'],
+        'options': {
+            'defaultType': 'future',
+        },
+        'urls': {
+            'api': {
+                'public': 'https://testnet.binancefuture.com/fapi/v1',
+                'private': 'https://testnet.binancefuture.com/fapi/v1',
+            },
+        }
+    })
+
+    if config['EXCHANGES']['BINANCE-FUTURES']['TESTNET']:
+        exchange.set_sandbox_mode(True)
+
+if use_binance_spot:
+    print("Binance Spot is enabled!")
+    exchange_spot = ccxt.binance({
+        'apiKey': config['EXCHANGES']['BINANCE-SPOT']['API_KEY'],
+        'secret': config['EXCHANGES']['BINANCE-SPOT']['API_SECRET'],
+        'options': {
+            'defaultType': 'spot',
+        }
+    })
 
 @app.route('/webhook1', methods=['POST'])
 def webhook():
+    global current_position, current_side
     print("Hook Received!")
     data = json.loads(request.data)
     print(data)
@@ -96,18 +128,18 @@ def webhook():
 
     try:
         if data['exchange'] == 'binance-futures':
-            if exchange_manager.use_binance_futures:
+            if use_binance_futures:
                 if data['side'] in ['buy', 'sell']:
-                    if order_manager.can_open_order():
-                        response = create_order(data, exchange_manager.exchange)
-                        order_manager.current_position = 'open'
-                        order_manager.current_side = data['side']
+                    if can_open_order(current_position):
+                        response = create_order(data, exchange)
+                        current_position = 'open'
+                        current_side = data['side']
                     else:
                         raise ValueError("Cannot open a new order until the current one is closed.")
                 elif data['side'] in ['closelong', 'closeshort']:
-                    if order_manager.can_close_order(data['side']):
-                        response = close_order(data, exchange_manager.exchange)
-                        order_manager.current_position = 'closed'
+                    if can_close_order(current_position, current_side, data['side']):
+                        response = close_order(data, exchange)
+                        current_position = 'closed'
                     else:
                         raise ValueError("Cannot close the order. Either there is no open order or the side of the closing order does not match the side of the open order.")
                 else:
@@ -117,12 +149,12 @@ def webhook():
                 raise ValueError("Binance Futures is not enabled in the config file.")
 
         elif data['exchange'] == 'binance-spot':
-            if exchange_manager.use_binance_spot:
+            if use_binance_spot:
                 if data['side'] in ['buy', 'sell']:
-                    if order_manager.can_open_order():
-                        response = create_order(data, exchange_manager.exchange_spot)
-                        order_manager.current_position = 'open'
-                        order_manager.current_side = data['side']
+                    if can_open_order(current_position):
+                        response = create_order(data, exchange_spot)
+                        current_position = 'open'
+                        current_side = data['side']
                     else:
                         raise ValueError("Cannot open a new order until the current one is closed.")
                 else:
@@ -132,18 +164,18 @@ def webhook():
                 raise ValueError("Binance Spot is not enabled in the config file.")
 
         elif data['exchange'] == 'bybit':
-            if exchange_manager.use_bybit:
+            if use_bybit:
                 if data['side'] in ['buy', 'sell']:
-                    if order_manager.can_open_order():
-                        response = create_order_bybit(data, exchange_manager.session)
-                        order_manager.current_position = 'open'
-                        order_manager.current_side = data['side']
+                    if can_open_order(current_position):
+                        response = create_order(data, session)
+                        current_position = 'open'
+                        current_side = data['side']
                     else:
                         raise ValueError("Cannot open a new order until the current one is closed.")
                 elif data['side'] in ['closelong', 'closeshort']:
-                    if order_manager.can_close_order(data['side']):
-                        response = close_order_bybit(data, exchange_manager.session)
-                        order_manager.current_position = 'closed'
+                    if can_close_order(current_position, current_side, data['side']):
+                        response = close_order(data, session)
+                        current_position = 'closed'
                     else:
                         raise ValueError("Cannot close the order. Either there is no open order or the side of the closing order does not match the side of the open order.")
                 else:
@@ -156,7 +188,7 @@ def webhook():
             raise ValueError("Unsupported exchange.")
 
     except Exception as e:
-        return error_handler.handle_error(e)
+        return handle_error(e)
 
 if __name__ == '__main__':
     app.run()
